9f7039763a2735ff22b3b8aea6568d42190cc36c
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-06-23 16:36:29 +0600

    90% complete

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ openBtn.addEventListener("click", () => {
     mobileNav.style.display = "block";
     mobileNav.style.top = "0"
 })
+
+
+
+// ----------------------image-------------- 
+
+
+
+
+/* accordion*/
+const item = document.getElementsByClassName("accordion");
+// console.log(item);
+const show = document.getElementsByClassName("show");
+const hide = document.getElementsByClassName("hide");
+
+for( let i = 0; i<item.length; i++){
+    item[i].addEventListener("click", function myAcc(){
+       let accResult = item[i].classList.toggle("active");
+
+       if(accResult == true){
+        show[i].style.display = "none";
+        hide[i].style.display = "block";
+       }else{
+        show[i].style.display = "block";
+        hide[i].style.display = "none";
+       }
+    })
+    
+}
